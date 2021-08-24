8f13a4bffb33f7b27dcfc21c36549e33c3256959
AlixBoufflers <79104398+AlixBoufflers@users.noreply.github.com>
2021-08-24 16:54:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const cors = require('cors')
 let app = express();
 
 app.use(cors())
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/tourism/points', (req,res) => {
     res.status(200).json(data)
 });
